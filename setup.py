6ed6a0e1555ee3515b6ff8543f5b983814052d53
David Koblas <koblas@extra.com>
2011-12-06 12:09:13 -0800

    Trying to get pip happy

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@
 
 from distutils.core import setup
 
+install_requires = []
+
 version = "0.1"
 
 setup(
@@ -11,6 +13,7 @@ setup(
     author='David Koblas',
     author_email='david@koblas.com',
     url='http://github.com/koblas/thistle',
+    install_requires=install_requires,
     package_dir = {'thistle' : 'py/thistle' },
     packages=['thistle'],
 )
